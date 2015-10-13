da1fe1cd4bba8f6a92eb4d8e2aaf13f26b5b9ec2
OZAWA Yasuhiro <a_house_of_tomato@lion.ocn.ne.jp>
2015-10-13 21:17:47 +0900

    プロジェクトを取得する処理を作成

--- js/redmine.js
+++ js/redmine.js
@@ -9,24 +9,47 @@ var RedmineClient = function(url) {
 
 RedmineClient.prototype = {
     login: function(login_id, password) {
-        var that = this;
+        this.login_id = login_id;
+        this.password = password;
         $.ajax({
             url: this.base_url + "/users/current.json",
             dataType: 'json',
             headers: {
                 "Authorization": "Basic " + btoa(login_id + ":" + password)
             },
-            success: this.set_account,
-            error: function(jqXHR, textStatus, errorThrown){
-                alert(textStatus + ": " + errorThrown);
-            }
+            success: this.setAccount.bind(this),
+            error: this.errorPattern
         });
     },
 
-    set_account: function(user) {
+    setAccount: function(user) {
         this.api_key = user['user']['api_key'];
         this.user_firstname = user['user']['firstname'];
         this.user_lastname = user['user']['lastname'];
         $('#account').text(this.user_firstname + ' ' + this.user_lastname);
+    },
+
+    getProjects: function() {
+        $.ajax({
+            url: this.base_url + "/projects.json",
+            dataType: 'json',
+            headers: {
+                "X-Redmine-API-Key": this.api_key
+            },
+            success: this.setProjects,
+            error: this.errorPattern
+        });
+    },
+
+    setProjects: function(projects) {
+        $.each(projects["projects"], function(index, value) {
+            var tag = $('<div>');
+            tag.text(value["name"]);
+            $('#project_list').append(tag);
+        });
+    },
+
+    errorPattern: function(jqXHR, textStatus, errorThrown) {
+        alert(textStatus + ": " + errorThrown);
     }
 };
