307e63dd549b345548b583727dfcf3afee897659
OZAWA Yasuhiro <a_house_of_tomato@lion.ocn.ne.jp>
2015-10-14 21:46:36 +0900

    チケット取得まで作成

--- js/redmine.js
+++ js/redmine.js
@@ -44,9 +44,53 @@ RedmineClient.prototype = {
     setProjects: function(projects) {
         $.each(projects["projects"], function(index, value) {
             var tag = $('<div>');
-            tag.text(value["name"]);
+            var link = $('<a>');
+
+            link.text(value["name"]);
+            link.attr('href', '#');
+
+            var identifier = $('<div>');
+            identifier.text(value["identifier"]);
+            identifier.addClass('attr');
+            identifier.addClass('identifier');
+
+            var number = $('<div>');
+            number.text(value["id"]);
+            number.addClass('attr');
+            number.addClass('project_id');
+
+            tag.addClass('project');
+            tag.append(link);
+            tag.append(number);
+            tag.append(identifier);
             $('#project_list').append(tag);
         });
+
+        $('#project_list div.project').click(function(that) {
+            // todo clientでなくthis的な呼び方をしたい
+            client.getIssues($(this).find('.project_id:first').text());
+        });
+    },
+
+    getIssues: function(project_id) {
+          $.ajax({
+            url: this.base_url + "/issues.json",
+            dataType: 'json',
+            headers: {
+                "X-Redmine-API-Key": this.api_key
+            },
+            data: {'project_id': project_id},
+            success: this.setIssues,
+            error: this.errorPattern
+        });
+    },
+
+    setIssues: function(issues) {
+      $.each(issues["issues"], function(index, issue) {
+          var tag = $('<div>');
+          tag.text(issue['subject']);
+          $('#issue_list').append(tag);
+      });
     },
 
     errorPattern: function(jqXHR, textStatus, errorThrown) {
